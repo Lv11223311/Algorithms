61d6ced6190c8880b9f863b4cddf429c21b06510
Lv11223311 <2695484680@qq.com>
2018-10-25 23:37:30 +0800

    add an Introduction into Algorm_Toolbox

---
+++ Algorm_Toolbox/README.py
@@ -0,0 +1,30 @@
+Algorithms Toolbox
+week 1 GOAL:
+Practice implementing algorithms
+Practice testing and stress testing programs
+Compare fast and slow programs
+Practice solving programming challenges
+
+week 2 GOAL:
+Estimate the running time of an algorithm
+Practice implementing efficient solutions
+Practice solving programming challenges
+Implement programs that are several orders of magnitude faster than straightforward programs
+
+week 3 Goal:
+Practice implementing greedy solutions
+Build greedy algorithms
+Create a program for changing money optimally
+Create a program for maximizing the value of a loot
+Create a program for maximizing the number of prize places in a competition
+
+week 4 Goal:
+Express the recurrence relation on the running time of an algorithm
+Create a program for searching huge lists
+Create a program for finding a majority element
+Create a program for organizing a lottery
+
+week 5 Goal:
+apply dynamic programming technique to implement efficient programs
+compute the edit distance between to files
+practice applying the most popular algorithmic technique: dynamic programming
